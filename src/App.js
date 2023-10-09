871471c10da2da4567a63de24796d0701af6f774
ANKUSH PATIL <patil.ankush28@gmail.com>
2023-10-09 14:43:56 +0530

    Added maps page and other navigation components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Users from './user/pages/Users';
-import NewPlace from './user/pages/NewPlace';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from "./places/pages/UserPlaces";
 function App() {
   return (
     <>
@@ -12,6 +13,7 @@ function App() {
           <main>
             <Routes>
               <Route path='/' element={<Users />} />
+              <Route path="/:userId/places" element={<UserPlaces />} exact />
               <Route path='/places/new' element={<NewPlace />} />
               <Route path='*' element={<Users />} />
             </Routes>
---
+++ src/places/components/PlaceList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "../styles/PlaceList.css";
+import Card from '../../shared/components/UIElements/Card';
+import PlaceItem from "../components/PlaceItem";
+const PlaceList = (props) => {
+    if (props.items.length === 0) {
+        return <div className="place-list-center">
+            <Card>
+                <h2>No places found. Maybe create one?</h2>
+            </Card>
+            <button>Share Place</button>
+        </div>
+    }
+    else {
+        return (
+            <ul className="place-list">
+                {props.items.map((place) => {
+                    return <PlaceItem
+                        key={place.id}
+                        id={place.id}
+                        image={place.imageUrl}
+                        title={place.title}
+                        description={place.description}
+                        address={place.address}
+                        creatorId={place.creator}
+                        coordinates={place.location}
+                    />
+                })}
+            </ul>
+        );
+    }
+};
+
+export default PlaceList;
