1f6d7b89316684cbc05c68b39e110945556a64f9
ANKUSH PATIL <patil.ankush28@gmail.com>
2023-10-11 17:38:32 +0530

    Feature - Added event handlers for Input field

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useCallback } from "react";
 import "../styles/NewPlace.css";
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from '../../shared/util/Validators';
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/Validators';
 const NewPlace = () => {
+    const titleInputHandler = useCallback((id, value, isValid) => {
+
+    }, []);
+    const descriptionInputHandler = useCallback((id, value, isValid) => {
+
+    }, []);
     return <form className="place-form">
-        <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title" />
+        <Input
+            id="title"
+            element="input"
+            type="text"
+            label="Title"
+            validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid title"
+            onInput={titleInputHandler}
+        />
+        <Input
+            id="description"
+            element="textarea"
+            label="Description"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a valid description(At least 5 characters)"
+            onInput={descriptionInputHandler}
+        />
     </form>
 };
 
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -26,9 +26,12 @@ function Input(props) {
         dispatch({ type: "CHANGE", val: event.target.value, validators: props.validators });
     }
 
+    const { id, onInput } = props;
+    const { value, isValid } = inputState;
+
     useEffect(() => {
-       
-    }, [input]);
+        onInput(id, value, isValid);
+    }, [id, value, isValid, onInput]);
 
     const touchHandeler = () => {
         dispatch({ type: "TOUCH" });
